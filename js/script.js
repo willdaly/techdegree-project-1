ecbcc0a221ed49482e833bb0d7b532c9c1ef098b
Will Daly <rwilliamdaly@gmail.com>
2018-12-10 15:26:38 -0500

    added more properties to the quote object

--- js/script.js
+++ js/script.js
@@ -2,27 +2,32 @@ const quotes = [
   {
     quote: `While breathing in long, one knows: "I breathe in long". While breathing out long, one knows "I breathe out long."`,
     source: "Gautama Buddha",
-    citation: "anapanasati sutta"
+    citation: "Anapanasati Sutta",
+    tetrad: "First Tetrad (Body Group)"
   },
   {
     quote: `While breathing in short, one knows: "I breathe in short." While breathing out short, one knows, "I breathe out short."`,
     source: "Gautama Buddha",
-    citation: "anapanasati sutta"
+    citation: "Anapanasati Sutta",
+    tetrad: "First Tetrad (Body Group)"
   },
   {
     quote: `One trains oneself: "Sensitive to the whole body, I breathe in. Sensitive to the whole body, I breathe out."`,
     source: "Gautama Buddha",
-    citation: "anapanasati sutta"
+    citation: "Anapanasati Sutta",
+    tetrad: "First Tetrad (Body Group)"
   },
   {
     quote: `One trains oneself: "Calming the whole body, I breathe in. Calming the whole body, I breathe out."`,
     source: "Gautama Buddha",
-    citation: "anapanasati sutta"
+    citation: "Anapanasati Sutta",
+    tetrad: "First Tetrad (Body Group)"
   },
   {
     quote: `One trains oneself: "Sensitive to rapture, I breathe in. Sensitive to rapture, I breathe out."`,
     source: "Gautama Buddha",
-    citation: "anapanasati sutta"
+    citation: "Anapanasati Sutta",
+    tetrad: "Second Tetrad (Feelings Group)"
   }
 ]; //array of quote objects
 
@@ -36,7 +41,7 @@ function printQuote () {
   let htmlString = `<p class="quote">${quoteObj.quote}</p><p class="source">${quoteObj.source}`; //html string with quote and source
   for (key in quoteObj) {
     if (key !== "quote" && key !== "source"){
-      htmlString += `<span class="${key}">${quoteObj[key]}</span>`;
+      htmlString += `<span class="${key}">${quoteObj[key]}</span> `;
     } //filter out quote and source properties
   } //add on spans for additional attribution elements
   document.getElementById('quote-box').innerHTML = htmlString+='</p>'; //set quote-box to html string
